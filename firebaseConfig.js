4d3544c4d353b13d6a1650c0967d908cbb7d0fc6
Eli Brown-Henderson <Eli.i.henderson95@gmail.com>
2024-04-11 10:03:37 -0400

    Update: movie details text
    movie details now includes text that tells the user to make a rating to see the rest of the comments.

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,6 +1,5 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import {
   FIREBASE_API_KEY,
   FIREBASE_AUTH_DOMAIN,
@@ -27,4 +26,5 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+export default app;
--- src/components/homeCards/AddCard.js
+++ src/components/homeCards/AddCard.js
@@ -18,21 +18,21 @@ const newGroup = {
 };
 
 const AddCard = () => {
-  const db = getFirestore(app); // Initialize Firestore
+  //r const db = getFirestore(app); // Initialize Firestore
 
-  const addGroup = async (groupData) => {
-    console.log("Attempting to add group...");
+  // const addGroup = async (groupData) => {
+  //   console.log("Attempting to add group...");
 
-    try {
-      const docRef = doc(db, "groups", groupData.name); // Corrected to define docRef
-      await setDoc(docRef, groupData);
+  //   try {
+  //     const docRef = doc(db, "groups", groupData.name); // Corrected to define docRef
+  //     await setDoc(docRef, groupData);
 
-      console.log("Document successfully written!");
-      console.log("Group reference: ", docRef); // Corrected log
-    } catch (error) {
-      console.error("Error adding document: ", error);
-    }
-  };
+  //     console.log("Document successfully written!");
+  //     console.log("Group reference: ", docRef); // Corrected log
+  //   } catch (error) {
+  //     console.error("Error adding document: ", error);
+  //   }
+  // };
 
   return <Button title="Add Group" onPress={() => addGroup(newGroup)} />;
 };
